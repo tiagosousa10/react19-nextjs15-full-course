1da0c8cb184efd11228d42c81b07f58ed7d76483
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-01 18:24:42 +0000

    css for theme and work with children props

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,4 +1,5 @@
 //components
+import { TimerIcon } from "lucide-react";
 import { Heading } from "./components/Heading";
 
 //css
@@ -8,11 +9,12 @@ import "./styles/theme.css";
 export function App() {
   return (
     <>
-      <Heading attr={123} attr2={"String"}>
-        Ola mundo 1
+      <Heading>
+        Hello
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
-      <Heading>Ola mundo 2</Heading>
-      <Heading>Ola mundo 3</Heading>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, aut.</p>
     </>
   );
