97cd5be39da7fb5064013f591f766ef8609e0d70
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-01 18:08:47 +0000

    working with module css and props

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//components
+import { Heading } from "./components/Heading";
 
-function App() {
-  const [count, setCount] = useState(0)
+//css
+import "./styles/global.css";
+import "./styles/theme.css";
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Heading attr={123} attr2={"String"}>
+        Ola mundo 1
+      </Heading>
+      <Heading>Ola mundo 2</Heading>
+      <Heading>Ola mundo 3</Heading>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, aut.</p>
     </>
-  )
+  );
 }
-
-export default App
