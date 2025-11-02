a3d1bd572cee2876cb2664473e0da98945eeab77
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-02 16:45:14 +0000

    countdown and form , fist jsx

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,5 +1,6 @@
 //components
 import { Container } from "./components/Container";
+import { CountDown } from "./components/CountDown";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
@@ -16,6 +17,32 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form action="" className="form">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input type="text" id="input" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
