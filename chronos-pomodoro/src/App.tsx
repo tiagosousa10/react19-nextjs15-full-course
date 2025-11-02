417b9f490d042a0655f2464b4dd48ef23d0f5a8c
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-02 16:05:11 +0000

    container component and css

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,21 +1,20 @@
 //components
-import { TimerIcon } from "lucide-react";
-import { Heading } from "./components/Heading";
 
 //css
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Hello
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, aut.</p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
