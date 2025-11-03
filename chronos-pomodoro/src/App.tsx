719f05248f5de66bcb3ed4da0e4e7be2b8184271
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-03 19:28:02 +0000

    create default button component

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,7 +1,9 @@
 //components
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
 import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
 import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
@@ -40,7 +42,8 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color="green" />
+            <DefaultButton icon={<StopCircleIcon />} color="red" />
           </div>
         </form>
       </Container>
