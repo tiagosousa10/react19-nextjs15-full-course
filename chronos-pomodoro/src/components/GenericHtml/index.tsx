c9247ccbff3ebaaedbdb6313531d9d918f9be897
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-04 21:18:09 +0000

    notfound & generic html

---
+++ chronos-pomodoro/src/components/GenericHtml/index.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+import styles from "./styles.module.css";
+
+type GenericHtmlProps = {
+  children: React.ReactNode;
+};
+
+export function GenericHtml({ children }: GenericHtmlProps) {
+  return <div className={styles.genericHtml}>{children}</div>;
+}
