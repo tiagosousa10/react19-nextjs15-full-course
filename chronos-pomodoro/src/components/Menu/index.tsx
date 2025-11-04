7df4d067a347e550a8a350cfb261e950cebb2db3
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-04 19:33:18 +0000

    useEffect and localstorage to save themes

--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,11 +1,27 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
 import styles from "./styles.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>("dark");
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem("theme") as AvailableThemes) || "dark";
+
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
@@ -16,9 +32,12 @@ export function Menu() {
 
       return nextTheme;
     });
-
-    document.documentElement.setAttribute("data-theme", theme); //muda o tema no html
   }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme); //muda o tema no html
+    localStorage.setItem("theme", theme);
+  }, [theme]); //executado qnd o valor the them muda
 
   return (
     <nav className={styles.menu}>
@@ -54,7 +73,7 @@ export function Menu() {
         title="Mudar Tema"
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
