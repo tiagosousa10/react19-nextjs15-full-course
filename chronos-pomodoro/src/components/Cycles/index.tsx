c5f881ff2d650c9962fe9fc002685b913277f99c
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-07 18:07:14 +0000

    configure next cycle jsx

--- chronos-pomodoro/src/components/Cycles/index.tsx
+++ chronos-pomodoro/src/components/Cycles/index.tsx
@@ -1,19 +1,38 @@
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 import styles from "./styles.module.css";
 
 export function Cycles() {
+  const { state } = useTaskContext();
+
+  const cycleSteps = Array.from({ length: state.currentCycle });
+  console.log("🚀 ~ Cycles ~ cycleSteps:", cycleSteps);
+
+  const cycleDescriptionMap = {
+    workTime: "Foco",
+    shortBreakTime: "Pausa curta",
+    longBreakTime: "Pausa longa",
+  };
+
   return (
     <div className={styles.cycles}>
       <span>Ciclos: </span>
 
       <div className={styles.cycleDots}>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.longBreakTime}`}></span>
+        {cycleSteps.map((_, index) => {
+          const nextCycle = getNextCycle(index);
+          const nextCycleType = getNextCycleType(nextCycle);
+
+          return (
+            <span
+              key={nextCycle}
+              className={`${styles.cycleDot} ${styles[nextCycleType]}`}
+              aria-label={`Indica de ciclo de ${cycleDescriptionMap[nextCycleType]}`}
+              title={`Indica de ciclo de ${cycleDescriptionMap[nextCycleType]}`}
+            ></span>
+          );
+        })}
       </div>
     </div>
   );
--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -7,6 +7,7 @@ import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formattedSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
   const { setState, state } = useTaskContext();
@@ -35,7 +36,7 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
+      duration: state.config[nextCycleType],
       type: nextCycleType,
     };
 
@@ -48,7 +49,7 @@ export function MainForm() {
         activeTask: newTask,
         currentCycle: nextCycle,
         secondsRemaining,
-        formattedSecondsRemaining: "00:00",
+        formattedSecondsRemaining: formattedSecondsToMinutes(secondsRemaining),
         tasks: [...prevState.tasks, newTask],
       };
     });
@@ -69,10 +70,11 @@ export function MainForm() {
         <p>Proximo intervalo é de 25min</p>
       </div>
 
-      <div className="formRow">
-        <Cycles />
-      </div>
-
+      {state.currentCycle === 0 && (
+        <div className="formRow">
+          <Cycles />
+        </div>
+      )}
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon />} color="green" />
       </div>
