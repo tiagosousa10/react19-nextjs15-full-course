fdd38c8163203bf151b0a76d44b30f37188e7e45
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-11 13:51:20 +0000

    install react-router and create first routes

--- chronos-pomodoro/src/components/Footer/index.tsx
+++ chronos-pomodoro/src/components/Footer/index.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router";
 import styles from "./styles.module.css";
 
 export function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <a href="">Entenda como funciona a tecnica pomodoro</a>
-      <a href="">
-        Chronos Pomodor &copy; {new Date().getFullYear()} - Feito com 💗{" "}
-      </a>
-    </footer>
-  );
+    return (
+        <footer className={styles.footer}>
+            <Link to="/about-pomodoro">
+                Entenda como funciona a tecnica pomodoro
+            </Link>
+            <Link to="/">
+                Chronos Pomodor &copy; {new Date().getFullYear()} - Feito com 💗{" "}
+            </Link>
+        </footer>
+    );
 }
--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,80 +1,81 @@
 import {
-  HistoryIcon,
-  HouseIcon,
-  MoonIcon,
-  SettingsIcon,
-  SunIcon,
+    HistoryIcon,
+    HouseIcon,
+    MoonIcon,
+    SettingsIcon,
+    SunIcon,
 } from "lucide-react";
 import styles from "./styles.module.css";
 import { useState, useEffect } from "react";
+import { Link } from "react-router";
 
 type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>(() => {
-    const storageTheme =
-      (localStorage.getItem("theme") as AvailableThemes) || "dark";
+    const [theme, setTheme] = useState<AvailableThemes>(() => {
+        const storageTheme =
+            (localStorage.getItem("theme") as AvailableThemes) || "dark";
 
-    return storageTheme;
-  });
-
-  const nextThemeIcon = {
-    dark: <SunIcon />,
-    light: <MoonIcon />,
-  };
-
-  function handleThemeChange(
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-  ) {
-    event.preventDefault(); //nao segue o link
-    setTheme((prevTheme) => {
-      const nextTheme = prevTheme === "dark" ? "light" : "dark";
-
-      return nextTheme;
+        return storageTheme;
     });
-  }
 
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme); //muda o tema no html
-    localStorage.setItem("theme", theme);
-  }, [theme]); //executado qnd o valor the them muda
+    const nextThemeIcon = {
+        dark: <SunIcon />,
+        light: <MoonIcon />,
+    };
 
-  return (
-    <nav className={styles.menu}>
-      <h1>{theme}</h1>
-      <a
-        href="#"
-        className={styles.menuLink}
-        aria-label="Ir para a Home"
-        title="Ir para a Home"
-      >
-        <HouseIcon />
-      </a>
-      <a
-        href="#"
-        className={styles.menuLink}
-        aria-label="Ver Historico"
-        title="Ver Historico"
-      >
-        <HistoryIcon />
-      </a>
-      <a
-        href="#"
-        className={styles.menuLink}
-        aria-label="Configurações"
-        title="Configurações"
-      >
-        <SettingsIcon />
-      </a>
-      <a
-        href="#"
-        className={styles.menuLink}
-        aria-label="Mudar Tema"
-        title="Mudar Tema"
-        onClick={handleThemeChange}
-      >
-        {nextThemeIcon[theme]}
-      </a>
-    </nav>
-  );
+    function handleThemeChange(
+        event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+    ) {
+        event.preventDefault(); //nao segue o link
+        setTheme((prevTheme) => {
+            const nextTheme = prevTheme === "dark" ? "light" : "dark";
+
+            return nextTheme;
+        });
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme); //muda o tema no html
+        localStorage.setItem("theme", theme);
+    }, [theme]); //executado qnd o valor the them muda
+
+    return (
+        <nav className={styles.menu}>
+            <h1>{theme}</h1>
+            <Link
+                to="/"
+                className={styles.menuLink}
+                aria-label="Ir para a Home"
+                title="Ir para a Home"
+            >
+                <HouseIcon />
+            </Link>
+            <a
+                href="#"
+                className={styles.menuLink}
+                aria-label="Ver Historico"
+                title="Ver Historico"
+            >
+                <HistoryIcon />
+            </a>
+            <a
+                href="#"
+                className={styles.menuLink}
+                aria-label="Configurações"
+                title="Configurações"
+            >
+                <SettingsIcon />
+            </a>
+            <a
+                href="#"
+                className={styles.menuLink}
+                aria-label="Mudar Tema"
+                title="Mudar Tema"
+                onClick={handleThemeChange}
+            >
+                {nextThemeIcon[theme]}
+            </a>
+        </nav>
+    );
 }
