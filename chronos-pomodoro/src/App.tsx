aa59dadb0917e3cfeb5af50f58b6765891a06936
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-12 11:43:52 +0000

    main router created with scroll to top effect

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,30 +1,18 @@
 //components
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
-import { Home } from "./pages/Home";
 import { MessagesContainer } from "./components/MessagesContainer";
+import { MainRouter } from "./routers/MainRouter";
 
 //css
 import "./styles/global.css";
 import "./styles/theme.css";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { NotFound } from "./pages/NotFound";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
 
 export function App() {
-    return (
-        <TaskContextProvider>
-            <MessagesContainer>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route
-                            path="/about-pomodoro"
-                            element={<AboutPomodoro />}
-                        />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </BrowserRouter>
-            </MessagesContainer>
-        </TaskContextProvider>
-    );
+  return (
+    <TaskContextProvider>
+      <MessagesContainer>
+        <MainRouter />
+      </MessagesContainer>
+    </TaskContextProvider>
+  );
 }
