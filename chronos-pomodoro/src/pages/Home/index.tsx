ced1531e7b3d562f09c38db92ef601aad214293c
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-12 18:03:44 +0000

    history pages first jsx and css

--- chronos-pomodoro/src/pages/Home/index.tsx
+++ chronos-pomodoro/src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
-import type { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
 
 export function Home() {
