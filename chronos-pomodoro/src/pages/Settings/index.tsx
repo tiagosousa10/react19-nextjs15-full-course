813bb308895cd923d64d875b83442ba762c1f417
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-13 19:21:53 +0000

    create form for settings

--- chronos-pomodoro/src/pages/Settings/index.tsx
+++ chronos-pomodoro/src/pages/Settings/index.tsx
@@ -1,4 +1,7 @@
+import { SaveIcon } from "lucide-react";
 import { Container } from "../../components/Container";
+import { DefaultButton } from "../../components/DefaultButton";
+import { DefaultInput } from "../../components/DefaultInput";
 import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
 
@@ -8,11 +11,34 @@ export function Settings() {
       <Container>
         <Heading>Configurações</Heading>
       </Container>
+
       <Container>
         <p style={{ textAlign: "center" }}>
           Modifique as configurações para tempo de foco, pausas, descanso curto
           e descanso longo
         </p>
+      </Container>
+
+      <Container>
+        <form action="form">
+          <div className="formRow">
+            <DefaultInput id="workTime" labelText="foco" />
+          </div>
+          <div className="formRow">
+            <DefaultInput id="shortBreakTime" labelText="Descanso curto" />
+          </div>
+          <div className="formRow">
+            <DefaultInput id="longBreakTime" labelText="Descanso longo" />
+          </div>
+
+          <div className="formRow">
+            <DefaultButton
+              icon={<SaveIcon />}
+              aria-label="Guardar Configuraçoes"
+              title="Guardar Configuraçoes"
+            />
+          </div>
+        </form>
       </Container>
     </MainTemplate>
   );
