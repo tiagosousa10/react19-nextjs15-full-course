023b18fbc2b3530c008062ede94990ac0eaf6bea
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-17 19:36:42 +0000

    container, header and postHeading components created

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,14 +1,47 @@
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
+import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostsLists";
 import { SpinLoader } from "@/components/SpinLoader";
 import { postRepository } from "@/repositories/post";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div>
-      <header className="text-6xl font-bold text-center py-8">
-        <h1>Aqui é a header</h1>
-      </header>
+    <Container>
+      <Header />
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group ">
+        <Link href="#" className="w-full h-full overflow-hidden rounded-xl">
+          <Image
+            className="group-hover:scale-105 transition w-full h-full object-cover object-center"
+            src="/images/bryen_0.png"
+            alt="Titulo do Post"
+            width={1200}
+            height={700}
+            priority
+          />
+        </Link>
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time className="text-slate-600 text-sm" dateTime="2025-04-20">
+            2025-04-20
+          </time>
+          <PostHeading as="h1" url="#">
+            dwdawdwa
+          </PostHeading>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          </p>
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
@@ -17,6 +50,6 @@ export default async function HomePage() {
       <footer className="text-6xl font-bold text-center py-8">
         <h1>Aqui é o footer</h1>
       </footer>
-    </div>
+    </Container>
   );
 }
--- blog/src/components/Header/index.tsx
+++ blog/src/components/Header/index.tsx
@@ -1,18 +1,19 @@
-"use client";
-
 import clsx from "clsx";
+import Link from "next/link";
 
 export function Header() {
-  console.log("Header");
   return (
-    <h1
-      className={clsx(
-        true &&
-          "text-6xl font-bold hover:text-blue-50 hover:bg-blue-500 transition duration-1000 "
-      )}
-      onClick={() => alert(123)}
-    >
-      Ola dentro de page.tsx
-    </h1>
+    <header>
+      <h1
+        className={clsx(
+          "text-4xl/normal font-extrabold py-8",
+          "sm:text-5xl/normal sm:py-10",
+          "md:textl-6xl md:py-11",
+          "lg:text-7xl lg:py-12"
+        )}
+      >
+        <Link href="#">The Blog</Link>
+      </h1>
+    </header>
   );
 }
