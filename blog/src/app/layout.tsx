ef92b0779d62bb89402a5cefeefe1235bd6b2466
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-18 14:08:11 +0000

    update metadata to receive a title & create footer component

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "The blog - Next.js",
+  title: {
+    default: "The Blog - Next.js",
+    template: "%s | The Blog",
+  },
   description: "Descrição da pagina",
 };
 
@@ -14,7 +20,11 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
       <body className="">
-        <div className="">{children}</div>
+        <Container>
+          <Header />
+          {children}
+          <Footer />
+        </Container>
       </body>
     </html>
   );
--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,22 +1,18 @@
-import { Container } from "@/components/Container";
-import { Header } from "@/components/Header";
-import { PostCoverImage } from "@/components/PostCoverImage";
 import { PostFeatured } from "@/components/PostFeatured";
-import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostsLists";
 import { SpinLoader } from "@/components/SpinLoader";
-import { postRepository } from "@/repositories/post";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-      <PostFeatured />
+    <>
+      <Suspense fallback={<SpinLoader />}>
+        <PostFeatured />
+      </Suspense>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-    </Container>
+    </>
   );
 }
