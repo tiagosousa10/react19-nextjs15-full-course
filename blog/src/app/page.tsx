7e0b0e3936106d2e53ad444e71ba79f924c2b47a
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-18 13:51:14 +0000

    post featured component created

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import { PostCoverImage } from "@/components/PostCoverImage";
+import { PostFeatured } from "@/components/PostFeatured";
 import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostsLists";
 import { SpinLoader } from "@/components/SpinLoader";
@@ -11,29 +12,11 @@ export default async function HomePage() {
   return (
     <Container>
       <Header />
-
-      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group ">
-        <PostCoverImage
-          linkProps={{
-            href: "/post/asda",
-          }}
-          imagesProps={{
-            width: 1200,
-            height: 720,
-            src: "/images/bryen_9.png",
-            alt: "Alt da imagem",
-            priority: true,
-          }}
-        />
-      </section>
+      <PostFeatured />
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer className="text-6xl font-bold text-center py-8">
-        <h1>Aqui é o footer</h1>
-      </footer>
     </Container>
   );
 }
---
+++ blog/src/components/PostFeatured/index.tsx
@@ -0,0 +1,36 @@
+import { PostCoverImage } from "../PostCoverImage";
+import { PostSummary } from "../PostSummary";
+import { findAllPublicPosts } from "@/lib/post/queries";
+
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+
+  const slug = "qualquer";
+  const postLink = `/post/${slug}`;
+
+  return (
+    <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+      <PostCoverImage
+        linkProps={{
+          href: postLink,
+        }}
+        imagesProps={{
+          width: 1200,
+          height: 720,
+          src: post.coverImageUrl,
+          alt: post.title,
+          priority: true,
+        }}
+      />
+
+      <PostSummary
+        postLink={postLink}
+        postHeading="h1"
+        createdAt={post.createdAt}
+        title={post.title}
+        excerpt={post.excerpt}
+      />
+    </section>
+  );
+}
