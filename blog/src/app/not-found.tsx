5ed64583518313f2a29b236e69fae03dd405800b
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-19 14:09:05 +0000

    not found page

---
+++ blog/src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+
+export default function NotFoundPage() {
+  return (
+    <div
+      className={clsx(
+        "min-h-[320px] bg-slate-900 text-slate-100",
+        "mb-16 p-8 rounded-xl",
+        "flex items-center justify-center",
+        "text-center"
+      )}
+    >
+      <div>
+        <h1 className="text-7xl/tight  mb-4 font-extrabold">404</h1>
+        <p>Erro 404 - Página não encontrada.</p>
+      </div>
+    </div>
+  );
+}
